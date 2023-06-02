8d570f7c9cd6b53dc695c7c09abc170aa110fe65
Tianrui Qi <tianrui.qi@icloud.com>
2023-06-01 22:52:50 -0400

    replace dataGenerator.m to dataset.py

--- config.py
+++ config.py
@@ -1,19 +1,17 @@
 class Config:
     def __init__(self):
-        # Parameters for dataLoader and dataGenerator
-        self.num_sample  = 6000
-        self.num_train   = 5600
-        self.noised     = True  # Noised ? noised sample : clean sample
-        self.binary     = False  # Binary ? classification : regression
-
-        # Parameters for dataGeneratorHelper
-        # dimensional parameters that need to consider memory
-        self.dim_frame   = [64, 64, 64]  # row-column-(depth); yx(z)
-        self.up_sampling = [8, 8, 4]
-        # parameters that adjust distribution of sample
-        self.mol_range   = [0, 16]  # min, max number of moleculars per frame
-        self.max_std     = [3, 3, 3]  # adjust variance of moleculars
-        self.lum_range   = [1/512, 1]
-        # parameters for adding noise
-        self.noise_mu   = 0
-        self.noise_var  = 1/512
+        self.num_sample = 10000
+        self.num_train  = 7000
+        # dimensional config that need to consider memory
+        self.dim_frame  = [64, 64, 64]  # row-column-(depth)
+        self.up_sample  = [8, 8, 4]
+        # config for adjust distribution of molecular
+        self.mol_range  = [0, 16]  # min, max number of molecular per frame
+        self.std_range  = [0.5, 3]  # adjust variance of molecular
+        self.lum_range  = [1/512, 1]
+        # config for reducing resolution and adding noise
+        self.bitdepth   = 16
+        self.qe         = 0.82
+        self.sen        = 5.88
+        self.noise_mu   = self.lum_range[0]
+        self.noise_var  = 1/128
--- dataset.py
+++ dataset.py
@@ -1,49 +1,121 @@
 import numpy as np
-from scipy.stats import multivariate_normal
-
 import torch
 from torch.utils.data import Dataset
+from scipy.stats import multivariate_normal
+from typing import Tuple  # for type annotations only
 
-from config import Config
+class SimDataset(Dataset):
+    def __init__(self, config, num):
+        super(SimDataset, self).__init__()
 
-class MyDataset(Dataset):
-    def __init__(self, config):
-        super(MyDataset, self).__init__()
+        self.num        = int(num)
+        # dimensional config that need to consider memory
+        self.dim_frame  = np.array(config.dim_frame).astype(int)     # [D]
+        self.up_sample  = np.array(config.up_sample).astype(int)     # [D]
+        # config for adjust distribution of molecular
+        self.mol_range  = np.array(config.mol_range).astype(int)     # [2]
+        self.std_range  = np.array(config.std_range).astype(float)   # [2]
+        self.lum_range  = np.array(config.lum_range).astype(float)   # [2]
+        # config for adding camera noise
+        self.bitdepth   = int(config.bitdepth)
+        self.qe         = float(config.qe)
+        self.sen        = float(config.sen)
+        self.noise_mu   = float(config.noise_mu)
+        self.noise_var  = float(config.noise_var)
 
-        self.config = config
-        
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
+        mean_set, var_set, lum_set = self.generateParas()
+        frame = self.generateFrame(mean_set, var_set, lum_set)
+        frame = self.generateNoise(frame)
+        label = self.generateLabel(mean_set, lum_set)
 
-    def __getitem__(self):
-        return
+        return torch.from_numpy(frame), torch.from_numpy(label)
 
-    def __len__(self):
-        return
+    def __len__(self) -> int:
+        return self.num
     
-
     # Help function for __getitem__
 
-    def generateDataParas(self):
-        # load parameters we will use
-        dim_frame: np.ndarray = np.array(self.config.dim_frame)  # [2]
-        mol_range: np.ndarray = np.array(self.config.mol_range)  # [2]
-        max_std  : np.ndarray = np.array(self.config.max_std)    # [D]
-        lum_range: np.ndarray = np.array(self.config.lum_range)  # [2]
+    def generateParas(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        D = len(self.dim_frame)  # number of dimension, i.e., 2D/3D frame
+        N = np.random.randint(self.mol_range[0], self.mol_range[1] + 1)
 
-        D: int = len(dim_frame)
-        N: int = np.random.randint(mol_range[0], mol_range[1] + 1)
-
-        # mu set, [D * N]
-        mu_set  = np.random.rand(D, N) * (dim_frame.reshape(-1, 1) - 1)
+        # mean set, [D * N]
+        mean_set = np.random.rand(D, N) * (self.dim_frame.reshape(-1, 1) - 1)
         # variance set, [D * N]
-        var_set = (np.random.rand(D, N) * max_std.reshape(-1, 1)) ** 2
+        var_set  = np.random.rand(D, N)
+        var_set *= self.std_range[1] - self.std_range[0]
+        var_set += self.std_range[0]
+        var_set *= var_set
         # luminance set, [N]
-        lum_set = np.random.rand(N) * (lum_range[1] - lum_range[0])
-        lum_set += lum_range[0]
+        lum_set  = np.random.rand(N) * (self.lum_range[1] - self.lum_range[0])
+        lum_set += self.lum_range[0]
 
-        return mu_set, var_set, lum_set
+        return mean_set, var_set, lum_set
 
+    def generateFrame(self, mean_set, var_set, lum_set) -> np.ndarray:
+        D = len(self.dim_frame)  # number of dimension, i.e., 2D/3D frame
+        N = len(lum_set)         # number of molecular in this frame
 
-if __name__ == "__main__":
-    config = Config()
-    dataset = MyDataset(config)
-    dataset.generateDataParas()
+        frame = np.zeros(self.dim_frame)
+        for m in range(N):
+            # parameters of m molecular
+            mean = mean_set[:,m]
+            var  = var_set[:,m]
+            lum  = lum_set[m]
+
+            # take a slice around the mean where the radia is 5 * std
+            ra = np.ceil(2 * np.sqrt(var))  # radius, [D]
+            lo = np.floor(np.maximum(np.round(mean) - ra, np.zeros(D)))
+            up = np.ceil(np.minimum(np.round(mean) + ra, self.dim_frame - 1))
+
+            # build coordinate system of the slice
+            index     = [np.arange(l, u+1) for l, u in zip(lo, up)]
+            grid_cell = np.meshgrid(*index, indexing='ij')  
+            coord     = np.column_stack([c.ravel() for c in grid_cell])
+
+            # compute the probability density for each point/pixel in slice
+            pdf = multivariate_normal.pdf(coord, mean=mean, cov=np.diag(var))
+
+            # set the luminate
+            pdf = pdf * lum / np.amax(pdf)
+
+            # put the slice back to whole frame
+            frame[tuple(coord.astype(int).T)] += pdf
+
+        # prevent lum exceeding 1 or below 0
+        frame[frame > 1] = 1 
+        frame[frame < 0] = 0 
+
+        return frame
+    
+    def generateNoise(self, frame) -> np.ndarray:
+        frame *= 2**self.bitdepth - 1  # clean    -> gray
+        frame /= self.sen              # gray     -> electons
+        frame /= self.qe               # electons -> photons
+        # add shot noise / poisson noise
+        frame = np.random.poisson(frame, size=frame.shape).astype(np.float64)
+        frame *= self.qe               # photons  -> electons
+        frame *= self.sen              # electons -> gray
+        # reducing resolution casue by limit bitdepth when store data
+        frame  = np.round(frame)
+        frame /= 2**self.bitdepth - 1  # gray     -> noised
+        
+        # manually add noise to improve robustness of network
+        frame += np.random.normal(
+            self.noise_mu, self.noise_var, frame.shape) * np.random.rand(1)
+
+        # prevent lum exceeding 1 or below 0
+        frame[frame > 1] = 1 
+        frame[frame < 0] = 0 
+
+        return frame
+
+    def generateLabel(self, mean_set, lum_set) -> np.ndarray:
+        dim_frame_up = self.dim_frame * self.up_sample
+        mean_set_up  = mean_set * self.up_sample.reshape(-1, 1)
+        
+        label = np.zeros(dim_frame_up.astype(int))
+        label[tuple(np.round(mean_set_up).astype(int))] = lum_set
+        
+        return label
