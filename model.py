4618d8ce72da91f153198962b0755f5a47d87f44
Tianrui Qi <tianrui.qi@icloud.com>
2023-07-19 09:19:50 -0400

    Change attention to CBAM

--- model.py
+++ model.py
@@ -4,19 +4,19 @@ from torch import Tensor
 
 
 class _DualConv(nn.Module):
-    def __init__(self, in_channels, out_channels, dim):
+    def __init__(self, in_ch: int, out_ch: int, dim: int) -> None:
         super(_DualConv, self).__init__()
         if   dim == 2: Conv, BatchNorm = nn.Conv2d, nn.BatchNorm2d
         elif dim == 3: Conv, BatchNorm = nn.Conv3d, nn.BatchNorm3d
         else: raise ValueError("dim must be 2 or 3")
 
         self.conv1 = nn.Sequential(
-            Conv(in_channels, in_channels, 3, padding=1, bias=False),
-            BatchNorm(in_channels)
+            Conv(in_ch, in_ch, 3, padding=1, bias=False),
+            BatchNorm(in_ch)
         )
         self.conv2 = nn.Sequential(
-            Conv(in_channels, out_channels, 3, padding=1, bias=False),
-            BatchNorm(out_channels)
+            Conv(in_ch, out_ch, 3, padding=1, bias=False),
+            BatchNorm(out_ch)
         )
         self.relu = nn.ReLU()
 
@@ -29,27 +29,27 @@ class _DualConv(nn.Module):
 
 
 class _ResDualConv(nn.Module):
-    def __init__(self, in_channels, out_channels, dim):
+    def __init__(self, in_ch: int, out_ch: int, dim: int) -> None:
         super(_ResDualConv, self).__init__()
         if   dim == 2: Conv, BatchNorm = nn.Conv2d, nn.BatchNorm2d
         elif dim == 3: Conv, BatchNorm = nn.Conv3d, nn.BatchNorm3d
         else: raise ValueError("dim must be 2 or 3")
 
         self.conv1 = nn.Sequential(
-            Conv(in_channels, in_channels, 3, padding=1, bias=False),
-            BatchNorm(in_channels)
+            Conv(in_ch, in_ch, 3, padding=1, bias=False),
+            BatchNorm(in_ch)
         )
         self.conv2 = nn.Sequential(
-            Conv(in_channels, out_channels, 3, padding=1, bias=False),
-            BatchNorm(out_channels)
+            Conv(in_ch, out_ch, 3, padding=1, bias=False),
+            BatchNorm(out_ch)
         )
         self.relu = nn.ReLU()
 
         self.skip = nn.Sequential(
-            Conv(in_channels, out_channels, kernel_size=1, bias=False),
-            BatchNorm(out_channels),
+            Conv(in_ch, out_ch, 1, bias=False),
+            BatchNorm(out_ch),
         )
-        if in_channels == out_channels: self.skip = nn.Identity()
+        if in_ch == out_ch: self.skip = nn.Identity()
 
     def forward(self, x: Tensor) -> Tensor:
         residual = x
@@ -62,7 +62,7 @@ class _ResDualConv(nn.Module):
 
 
 class _UpConv(nn.Module):
-    def __init__(self, in_channels, out_channels, dim):
+    def __init__(self, in_ch: int, out_ch: int, dim: int) -> None:
         super(_UpConv, self).__init__()
         if   dim == 2: Conv, BatchNorm = nn.Conv2d, nn.BatchNorm2d
         elif dim == 3: Conv, BatchNorm = nn.Conv3d, nn.BatchNorm3d
@@ -70,57 +70,86 @@ class _UpConv(nn.Module):
 
         self.upconv = nn.Sequential(
             nn.Upsample(scale_factor=2),
-            Conv(in_channels, out_channels, 3, padding=1, bias=False),
-            BatchNorm(out_channels),
+            Conv(in_ch, out_ch, 3, padding=1, bias=False),
+            BatchNorm(out_ch),
             nn.ReLU()
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         return self.upconv(x)
 
 
-class _AttBlock(nn.Module):
-    def __init__(self, in_channels, n_coefficients, dim):
-        super(_AttBlock, self).__init__()
-        if   dim == 2: Conv, BatchNorm = nn.Conv2d, nn.BatchNorm2d
-        elif dim == 3: Conv, BatchNorm = nn.Conv3d, nn.BatchNorm3d
+class _ChannelAttentionModule(nn.Module):
+    def __init__(self, in_ch, dim: int, ratio: int = 16) -> None:
+        super(_ChannelAttentionModule, self).__init__()
+        if   dim == 2: 
+            Conv = nn.Conv2d
+            AdaptiveAvgPool = nn.AdaptiveAvgPool2d
+            AdaptiveMaxPool = nn.AdaptiveMaxPool2d
+        elif dim == 3:
+            Conv = nn.Conv3d
+            AdaptiveAvgPool = nn.AdaptiveAvgPool3d
+            AdaptiveMaxPool = nn.AdaptiveMaxPool3d
         else: raise ValueError("dim must be 2 or 3")
 
-        self.w_decod = nn.Sequential(
-            Conv(in_channels, n_coefficients, 1, bias=False),
-            BatchNorm(n_coefficients)
+        self.avg_pool = AdaptiveAvgPool(1)
+        self.max_pool = AdaptiveMaxPool(1)
+        self.MLP = nn.Sequential(
+            Conv(in_ch, in_ch // ratio, 1, bias=False),
+            nn.ReLU(),
+            Conv(in_ch // ratio, in_ch, 1, bias=False)
         )
-        self.w_encod = nn.Sequential(
-            Conv(in_channels, n_coefficients, 1, bias=False),
-            BatchNorm(n_coefficients)
-        )
-        self.relu = nn.ReLU()
-        self.psi = nn.Sequential(
-            Conv(n_coefficients, 1, 1, bias=False),
-            BatchNorm(1),
-            nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.sigmoid(
+            self.MLP(self.avg_pool(x)) + self.MLP(self.max_pool(x))
         )
 
-    def forward(self, encod, decod):
-        psi = self.w_encod(encod) + self.w_decod(decod)
-        psi = self.relu(psi)
-        psi = self.psi(psi)
-        return encod * psi
+
+class _SpatialAttentionModule(nn.Module):
+    def __init__(self, dim: int) -> None:
+        super(_SpatialAttentionModule, self).__init__()
+        if   dim == 2: Conv = nn.Conv2d
+        elif dim == 3: Conv = nn.Conv3d
+        else: raise ValueError("dim must be 2 or 3")
+
+        self.conv = Conv(2, 1, 7, padding=3)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x: Tensor) -> Tensor:
+        avgout = torch.mean(x, dim=1, keepdim=True)
+        maxout, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avgout, maxout], dim=1)
+        x = self.conv(x)
+        return self.sigmoid(x)
+
+
+class _CBAM(nn.Module):
+    def __init__(self, in_ch: int, dim: int) -> None:
+        super(_CBAM, self).__init__()
+        self.channel_attention = _ChannelAttentionModule(in_ch, dim)
+        self.spatial_attention = _SpatialAttentionModule(dim)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.channel_attention(x) * x
+        x = self.spatial_attention(x) * x
+        return x
 
 
 class _OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels, dim):
+    def __init__(self, in_ch: int, out_ch: int, dim: int) -> None:
         super(_OutConv, self).__init__()
         if   dim == 2: Conv = nn.Conv2d
         elif dim == 3: Conv = nn.Conv3d
         else: raise ValueError("dim must be 2 or 3")
 
         self.outconv   = nn.Sequential(
-            Conv(in_channels, out_channels, 1),
+            Conv(in_ch, out_ch, 1),
             nn.ReLU()
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         return self.outconv(x)
 
 
@@ -154,7 +183,7 @@ class ResAttUNet(nn.Module):
             for i in range(1, len(self.feats)-1)
         ])
         if self.attention: self.att = nn.ModuleList([
-            _AttBlock(self.feats[i], self.feats[i]//2, self.dim)
+            _CBAM(self.feats[i], self.dim)
             for i in range(1, len(self.feats)-1)
         ])
         self.decoder = nn.ModuleList([
